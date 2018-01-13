a54efe6f12487562928173540cfcae653a69e54c
ZHAO DAN <danielzhaochina@gmail.com>
2018-01-13 19:55:27 +0800

    UPD: similarweb and cleaned html

--- scraping/CleanHtml/script.py
+++ scraping/CleanHtml/script.py
@@ -6,7 +6,7 @@ import requests
 
 cookie = 'ajs_anonymous_id=%22176919ac-6f17-4c5f-bcdf-20cf3b97d65e%22; _first_pageview=1; _jsuid=3862967262; ajs_user_id=null; ajs_group_id=null; _ga=GA1.2.1216111861.1496139082; _gid=GA1.2.1682312280.1496139207; _drip_client_9331461=vid%253Dffceee50274d013562411277a4eb0864%2526pageViews%253D5%2526sessionPageCount%253D5%2526lastVisitedAt%253D1496139206945%2526weeklySessionCount%253D1%2526lastSessionAt%253D1496139083569; mp_ef8589eff3bf62c9dcac2779564de029_mixpanel=%7B%22distinct_id%22%3A%20%2215c58d6a80f260-034a4d5ef6bfff-3024410f-13c680-15c58d6a81083f%22%2C%22mp_lib%22%3A%20%22Segment%3A%20web%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D; mp_mixpanel__c=0; _gat=1; amplitude_iddiffbot.com=eyJkZXZpY2VJZCI6IjNhMjU3NjczLTUzZDUtNDExMi04MjkzLWYyMDg1MTUzNjk0YlIiLCJ1c2VySWQiOm51bGwsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTQ5NjEzOTA4MTYxNSwibGFzdEV2ZW50VGltZSI6MTQ5NjEzOTIwNzAwMSwiZXZlbnRJZCI6NSwiaWRlbnRpZnlJZCI6MCwic2VxdWVuY2VOdW1iZXIiOjV9; _hp2_ses_props.2423915105=%7B%22ts%22%3A1496139081984%2C%22d%22%3A%22www.diffbot.com%22%2C%22h%22%3A%22%2F%22%7D; _hp2_id.2423915105=%7B%22userId%22%3A%226606554637446143%22%2C%22pageviewId%22%3A%220515182173179074%22%2C%22sessionId%22%3A%221042976026160454%22%2C%22identity%22%3Anull%2C%22trackerVersion%22%3A%223.0%22%7D; __hstc=66047665.e05766620da0ed36aed902946bd3531d.1496139086082.1496139086082.1496139086082.1; __hssrc=1; __hssc=66047665.5.1496139086083; hubspotutk=e05766620da0ed36aed902946bd3531d; _eventqueue=%7B%22heatmap%22%3A%5B%5D%2C%22events%22%3A%5B%5D%7D; _vwo_uuid_v2=3187BC3B7AC5C456EAC86E5135924194|c286fcc6977e26218c9e1bac9b7199e3'
 alldata = []
-diff_token='024c9b27e91c72800533d7c889925667'
+diff_token='4250a98168a8fc9c3299a73780b74459'
 
 def get_request(get_url):
     headers = {
@@ -15,7 +15,7 @@ def get_request(get_url):
         'Referer': get_url,
         'Cookie': cookie,
     }
-    res_data = requests.get(get_url, headers=headers, timeout=10)
+    res_data = requests.get(get_url, headers=headers, timeout=5)
     return res_data
 
 
@@ -37,7 +37,7 @@ def get_cleaned_html(url):
         title = resp_obj.get('title', '')
         text = ''
     one_row = [url, title, text if text else '']
-    print url, title, len(text)
+    print 'SUCC', url, title, len(text)
     alldata.append(one_row)
     return True
 
@@ -48,17 +48,19 @@ def read_excel(filename, start):
     table = data.sheets()[0]
     for i in range(start, table.nrows):
         try:
-            # if i not in [332, 578, 634, 858, 930, 1041, 1042, ]:
+            # if i not in [1, 93, 107, 145, 161, 195, 372, 376, 501, 527, 593, 623, 641, 711, 1127, 1141, 1189, 1203, 1247, 1306, 1402, 1404, 1531, 1743, 1894, 2542, 2571, 2612, 2615, 2658, 2870, 2912, 3294, 3300, 4055, ]:
             #     continue
             url = table.row(i)[5].value.strip()
             flag = get_cleaned_html(url)
             if not flag:
                 break
-            time.sleep(1)
-        except:
-            print 'ERROR--' + str(i)
+            time.sleep(0.5)
+        except Exception as e:
+            print 'ERROR--' + str(i), e
             time.sleep(1)
             continue
+        if i > 10000:
+            break
 
 
 def write(html, filename):
@@ -81,5 +83,5 @@ def write_excel(filename, data):
     workbook.close()
     print filename + "===========over============"
 
-read_excel('data/sheet1.xls', 1)
-write_excel('data/sheet3.xls', alldata)
+read_excel('data/SG_sheet1.xls', 1)
+write_excel('data/SG_sheet3.xls', alldata)
--- scraping/similarweb/script.py
+++ scraping/similarweb/script.py
@@ -35,37 +35,41 @@ def write_excel(filename, data):
 
 def open_browser_scroll(url):
     global html_name
-    driver = webdriver.Chrome(executable_path=r'./chromedriver')  # Optional argument, if not specified will search path.
-    driver.get(url)
-    time.sleep(sleep_time)
-    html_source = driver.page_source
-    data = html_source.encode('utf-8').replace('\t', '').replace('\r', '').replace('\n', '')
-    driver.close()
+    try:
+        driver = webdriver.Chrome(executable_path=r'./chromedriver')  # Optional argument, if not specified will search path.
+        driver.get(url)
+        time.sleep(sleep_time)
+        html_source = driver.page_source
+        data = html_source.encode('utf-8').replace('\t', '').replace('\r', '').replace('\n', '')
+    except Exception as e:
+        raise e
+    finally:
+        driver.close()
     return data
 
 
 def request_sheet2(base_url):
     global sheet_dict, sleep_time
-    if sheet_dict.get(base_url):
-        return None
 
-    rank_reg = 'rankingItem--global.*?rankingItem-value.*?>(.*?)<.*?rankingItem--country.*?rankingItem-value.*?>(.*?)<.*?rankingItem--category.*?rankingItem-value.*?>(.*?)<.*?Total Visits(.*?)Traffic Source'
+    # rank_reg = 'rankingItem--global.*?rankingItem-value.*?>(.*?)<.*?rankingItem--country.*?rankingItem-value.*?>(.*?)<.*?rankingItem--category.*?rankingItem-value.*?>(.*?)<.*?Total Visits(.*?)Traffic Source'
+    rank_reg = 'js-globalRank.*?js-websiteRanksValue.*?>(.*?)</div.*?js-countryRank.*?js-websiteRanksValue.*?>(.*?)</div.*?js-categoryRank.*?js-websiteRanksValue.*?>(.*?)</div.*?websitePage-engagementInfoContainer.*?>(.*?)Engagement body'
     country_tag = 'accordion-toggle.*?countValue">(.*?)<.*?country-name.*?>(.*?)<'
 
     url = 'https://www.similarweb.com/website/' + base_url.replace('http://', '').replace('https://', '').split('www.')[-1]
     html = open_browser_scroll(url)
     global_ranks = re.compile(rank_reg).findall(html)
     if global_ranks:
-        sleep_time = 3
-        ret = [global_ranks[0][0].replace('[#,]', ''), global_ranks[0][1].replace('[#,]', ''), global_ranks[0][2].replace('[#,]', '')]
-        if 'countValue">' in global_ranks[0][3]:
-            count_value_reg = 'countValue">(.*?)<'
+        sleep_time = 1
+        ret = [remove_html_tag(global_ranks[0][0]), remove_html_tag(global_ranks[0][1]), remove_html_tag(global_ranks[0][2])]
+        if 'engagementInfo-valueNumber js-countValue' in global_ranks[0][3]:
+            count_value_reg = 'engagementInfo-valueNumber js-countValue">(.*?)<'
             count_value = re.compile(count_value_reg).findall(global_ranks[0][3])[0]
             ret.append(count_value)
         else:
             ret.append(0)
+
     else:
-        sleep_time = 20
+        sleep_time = 30
         ret = [0, 0, 0, 0]
 
     country_ranks = re.compile(country_tag).findall(html)
@@ -74,73 +78,10 @@ def request_sheet2(base_url):
         ret.append(country[0])
     if len(ret) == 4:
         ret += [0 for i in range(10)]
-    sheet_dict[base_url] = ret
     return ret
 
 
-def redo_scrape():
-    filename = 'data/sheet2.xls'
-    data = xlrd.open_workbook(filename, encoding_override="utf-8")
-    table = data.sheets()[0]
-    for i in range(1, table.nrows):
-        row = table.row(i)
-        try:
-            main_url = row[1].value
-            publisher = row[0].value
-            article_url = row[2].value
-            country = ''
-            if row[4].value == '0' or row[4].value == 0:
-                details = request_sheet2(main_url)
-                if not details:
-                    continue
-                one_row = [publisher, main_url, article_url, country] + details
-                print i, one_row
-                sheet2_data.append(one_row)
-            else:
-                one_row = [row[i].value for i in range(18)]
-                sheet2_data.append(one_row)
-        except:
-            print(i)
-    write_excel('data/sheet2_2.xls', sheet2_data)
-
-
-def mapping_local_file():
-    filename = 'data/sheet2.xls'
-    data = xlrd.open_workbook(filename, encoding_override="utf-8")
-    table = data.sheets()[0]
-    url_details = {}
-    for i in range(1, table.nrows):
-        row = table.row(i)
-        try:
-            main_url = row[1].value
-            if row[4].value != '0' and row[4].value != 0:
-                if not url_details.get(main_url):
-                    url_details[main_url] = [row[i].value for i in range(4, 18)]
-        except:
-            print(i)
-    for i in range(1, table.nrows-1):
-        row = table.row(i)
-        try:
-            main_url = row[1].value
-            publisher = row[0].value
-            article_url = row[2].value
-            country = ''
-            if row[4].value == '0' or row[4].value == 0:
-                details = url_details.get(main_url)
-                if not details:
-                    continue
-                one_row = [publisher, main_url, article_url, country] + details
-                print i, one_row
-                sheet2_data.append(one_row)
-            else:
-                one_row = [row[i].value for i in range(18)]
-                sheet2_data.append(one_row)
-        except:
-            print(i)
-    write_excel('data/sheet3.xls', sheet2_data)
-
-
-def read_excel(filename, start=1):
+def read_excel_filter_duplicated(filename, start=1):
     data = xlrd.open_workbook(filename, encoding_override="utf-8")
     table = data.sheets()[0]
 
@@ -151,20 +92,55 @@ def read_excel(filename, start=1):
             publisher = row[7].value
             article_url = row[5].value
             country = row[2].value
-            details = request_sheet2(main_url)
-            if not details:
+            if sheet_dict.get(main_url):
                 continue
-            one_row = [publisher, main_url, article_url, country] + details
-            print i, one_row
+            one_row = [publisher, main_url, article_url, country] + []
             sheet2_data.append(one_row)
+            sheet_dict[main_url] = True
         except Exception as e:
             print(i, e)
 
-redo_scrape()
-mapping_local_file()
+
+def remove_html_tag(ori):
+    dr = re.compile(r'<[^>]+>', re.S)
+    dd = dr.sub('', ori)
+    return str(HTMLParser.HTMLParser().unescape(dd)).strip()
 
 
-filename = 'data/sheet1.xls'
-# read_excel(filename, start=1,)
-# write_excel('data/sheet2.xls', sheet2_data)
+def read_excel_get_data(filename, filename_prefix, start=1, length=150):
+    global sheet2_data
+    data = xlrd.open_workbook(filename, encoding_override="utf-8")
+    table = data.sheets()[0]
+
+    for i in xrange(start, table.nrows):
+        row = table.row(i)
+        try:
+            global_rank = row[4].value
+            if global_rank == '' or global_rank == 0:
+                main_url = row[1].value
+                details = request_sheet2(main_url)
+                one_row = [row[0].value, row[1].value, row[2].value, row[3].value] + details
+                print i, one_row
+                sheet2_data.append(one_row)
+            else:
+                one_row = []
+                for j in xrange(table.ncols):
+                    one_row.append(row[j].value)
+                sheet2_data.append(one_row)
+            if i % length == 0:
+                write_excel('data/%s_%d.xls'%(filename_prefix, i), sheet2_data)
+                del sheet2_data
+                sheet2_data = [['Name of Publisher', 'Main url', 'Url of article', 'Country', 'Clobal Rank', 'Country Rank', 'Category Rank', 'Engagement', 'Top Country 1', 'Traffic 1', 'Top Country 2', 'Traffic 2', 'Top Country 3', 'Traffic 3', 'Top Country 4', 'Traffic 4', 'Top Country 5', 'Traffic 5']]
+        except Exception as e:
+            print i, e
+
+
+filename_prefix = 'GM_EN_sheet2'
+# filename = 'data/GM_sheet1.xls'
+# read_excel_filter_duplicated(filename, start=1)
+# write_excel('data/GM_sheet2.xls', sheet2_data)
+read_excel_get_data('data/%s.xls' % filename_prefix, filename_prefix, start=1, length=500)
+write_excel('data/%s_end.xls' % filename_prefix, sheet2_data)
+
+
 
