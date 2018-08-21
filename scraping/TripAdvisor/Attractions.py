ed39c9bc1f76ffb150f07527fd7ca13a53c30c2d
dan.zhao <dan.zhao@antfin.com>
2018-08-21 21:51:52 +0800

    TA attraction

--- scraping/TripAdvisor/Attractions.py
+++ scraping/TripAdvisor/Attractions.py
@@ -19,11 +19,11 @@ base_url = 'https://www.tripadvisor.com.sg/Attractions-g293961-Activities-Sri_La
 cookie = 'TASSK=enc%3AAEWqEz6PSzC%2B0mEdDhD8TYdzehbp3E5yYPS6IU82xNhZCWs42WFVupCN6TIq%2BDwmTQFLrFrGKXbjWkFV%2BOeOlqTuYZrVuC5PIwN05CmlOlt9VSjr08IEjSvs%2BcHUn1J3RA%3D%3D; ServerPool=B; TATravelInfo=V2*A.2*MG.-1*HP.2*FL.3*RS.1; TAUnique=%1%enc%3AdFWRrhvqgMB4WP%2FjIpESou4ikPqkoGPefimZEtCaTt9KQSq%2B20wPGQ%3D%3D; QSI_HistorySession=https%3A%2F%2Fwww.tripadvisor.com.sg%2FRestaurants~1533811522547; TAAuth3=3%3Aa1e20af987caf9b7f2e3d715d71cd8d4%3AABwR3pxNIStPrcA7Zi46RsfsujSNA3I7GJk169ieF9cfJlnAOA2sHRrbJzS0dcMxegnf%2FN%2F2U2NKFU4EB3yJpMbpPOZb0eIN1NJg17NlouXzWMFkTL9V4SbxrAuOo%2F9HH%2F9S6LS3VHPRXTVy1M7a7xFBn%2FenvwiN7zMUXibBkVTMgw533gBhKsJWI1hd1n61oumSV45qrk%2F5%2F6KYUFET4Jg%3D; PAC=AKlo368cQeby8aGk0VnZljir1BEGGqmVA3Iymn-e1QuFW7y7r8oArkHc2xiu8i9fNW-jeR4euvlJLrGHLDwmVxMmC7R4_R8p__XSvvg04kGJlUVAAvVt06GefL92kttoLw%3D%3D; PMC=V2*MS.96*MD.20180809*LD.20180819; TAReturnTo=%1%%2FAttractions-g298184-Activities-Tokyo_Tokyo_Prefecture_Kanto.html; TART=%1%enc%3Aw6QMi5j164r1aaWxJY9JxnCElH0GEYgfNKuWfmwSDQ5I%2FNSbY1dvaDmjnRZPfKWWF4MXNLqS0zE%3D; CM=%1%HanaPersist%2C%2C-1%7Cpu_vr2%2C%2C-1%7CHanaSession%2C%2C-1%7CRestAds%2FRPers%2C%2C-1%7CRCPers%2C%2C-1%7Cpv%2C2%2C-1%7Cpu_vr1%2C%2C-1%7CFtrPers%2C%2C-1%7Ctvsess%2C-1%2C-1%7CPremiumMCSess%2C%2C-1%7CRestPartSess%2C%2C-1%7Cbrandsess%2C%2C-1%7CRestPremRSess%2C%2C-1%7CPremRetPers%2C%2C-1%7CViatorMCPers%2C%2C-1%7Csesssticker%2C%2C-1%7C%24%2C%2C-1%7Ct4b-sc%2C%2C-1%7CMC_IB_UPSELL_IB_LOGOS2%2C%2C-1%7CPremMCBtmSess%2C%2C-1%7CLaFourchette+Banners%2C%2C-1%7CTARSWBPers%2C%2C-1%7CTheForkORSess%2C%2C-1%7CTheForkRRSess%2C%2C-1%7CRestAds%2FRSess%2C%2C-1%7CPremiumMobPers%2C%2C-1%7CLaFourchette+MC+Banners%2C%2C-1%7CRestPartPers%2C%2C-1%7Cvr_npu2%2C%2C-1%7CLastPopunderId%2C104-771-null%2C-1%7Csh%2C%2C-1%7CTheForkMCCSess%2C%2C-1%7Cvr_npu1%2C%2C-1%7CCCPers%2C%2C-1%7CWAR_RESTAURANT_FOOTER_SESSION%2C%2C-1%7Cb2bmcsess%2C%2C-1%7CSPMCPers%2C%2C-1%7CPremRetSess%2C%2C-1%7CViatorMCSess%2C%2C-1%7CPremiumMCPers%2C%2C-1%7CPremiumRRPers%2C%2C-1%7CRestAdsCCPers%2C%2C-1%7CWarPopunder_Persist%2C%2C-1%7Cr_ta_2%2C%2C-1%7Cr_ta_1%2C%2C-1%7CSPORPers%2C%2C-1%7Cperssticker%2C%2C-1%7CSPMCWBSess%2C%2C-1%7CPremiumMobSess%2C%2C-1%7Ct4b-pc%2C%2C-1%7CSPHRSess%2C%2C-1%7CWShadeSeen%2C%2C-1%7CTheForkMCCPers%2C%2C-1%7CHomeASess%2C4%2C-1%7CPremiumSURPers%2C%2C-1%7Ccatchsess%2C9%2C-1%7CCpmPopunder_1%2C%2C-1%7CCCSess%2C%2C-1%7CCpmPopunder_2%2C4%2C-1%7CRestAdsPers%2C%2C-1%7Cb2bmcpers%2C%2C-1%7CPremiumSURSess%2C%2C-1%7CMC_IB_UPSELL_IB_LOGOS%2C%2C-1%7Csess_rev%2C%2C-1%7Csessamex%2C%2C-1%7CPremiumRRSess%2C%2C-1%7CAdsRetPers%2C%2C-1%7CSaveFtrPers%2C%2C-1%7CSPMCSess%2C%2C-1%7Cpers_rev%2C%2C-1%7CMetaFtrSess%2C%2C-1%7CSPMCWBPers%2C%2C-1%7CRBAPers%2C%2C-1%7CWAR_RESTAURANT_FOOTER_PERSISTANT%2C%2C-1%7CFtrSess%2C%2C-1%7CHomeAPers%2C%2C-1%7C+r_lf_1%2C%2C-1%7CSPHRPers%2C%2C-1%7CRCSess%2C%2C-1%7C+r_lf_2%2C%2C-1%7Ccatchpers%2C3%2C1535292739%7CRestAdsCCSess%2C%2C-1%7CRestPremRPers%2C%2C-1%7Cpssamex%2C%2C-1%7Cbrandpers%2C%2C-1%7CAdsRetSess%2C%2C-1%7CWarPopunder_Session%2C%2C-1%7CSCA%2C%2C-1%7CTheForkORPers%2C%2C-1%7CPremMCBtmPers%2C%2C-1%7CTheForkRRPers%2C%2C-1%7CSaveFtrSess%2C%2C-1%7CTARSWBSess%2C%2C-1%7CRestAdsSess%2C%2C-1%7CRBASess%2C%2C-1%7CCPNC%2C%2C-1%7CMetaFtrPers%2C%2C-1%7C; roybatty=TNI1625!AFqR7ATFCf5BGUAXxbLKloOxq3ijkVYf0DRPU3MnogjptORJ20l7mvo9cz4qHCK1FtCZQYJpUlyB7gQAfYB%2B4UHck4ZZUD4ltlidfRM0Bir%2FYKDREbwphF7TnkEwk7vQj9vPd%2Fz%2BLDUMz5wEc8q%2BIycZYpJz3eceMRx60q3RfuiU%2C1; TASession=V2ID.075847DC4F66A5B0EA7DFFC570929807*SQ.185*LS.PageMoniker*GR.68*TCPAR.37*TBR.35*EXEX.55*ABTR.57*PHTB.99*FS.91*CPU.31*HS.recommended*ES.popularity*AS.popularity*DS.5*SAS.popularity*FPS.oldFirst*TS.C046DDFA89F7DF42D3E2657A089B8B5A*LF.en*FA.1*DF.0*MS.-1*RMS.-1*FLO.298184*TRA.true*LD.298184; TAUD=LA-1533811506689-1*RDD-1-2018_08_09*LG-876436396-2.1.F.*LD-876436397-.....'
 uid_level_dict = {}
 base_url_list = [
+    # ('https://www.tripadvisor.com.sg/Attractions-g298112-Activities-oa%s-Gifu_Gifu_Prefecture_Tokai_Chubu.html', 5, 'Gifu'),
+    # ('https://www.tripadvisor.com.sg/Attractions-g298106-Activities-oa%s-Nagoya_Aichi_Prefecture_Tokai_Chubu.html', 16, 'Nagoya'),
+    # ('https://www.tripadvisor.com.sg/Attractions-g298566-Activities-oa%s-Osaka_Osaka_Prefecture_Kinki.html', 44, 'Osaka'),
+    ('https://www.tripadvisor.com.sg/Attractions-g298564-Activities-oa%s-Kyoto_Kyoto_Prefecture_Kinki.html', 47, 'Kyoto'),    
     ('https://www.tripadvisor.com.sg/Attractions-g298184-Activities-oa%s-Tokyo_Tokyo_Prefecture_Kanto.html', 159, 'Tokyo'), 
-    ('https://www.tripadvisor.com.sg/Attractions-g298566-Activities-oa%s-Osaka_Osaka_Prefecture_Kinki.html', 44, 'Osaka'),
-    ('https://www.tripadvisor.com.sg/Attractions-g298564-Activities-oa%s-Kyoto_Kyoto_Prefecture_Kinki.html', 47, 'Kyoto'),
-    ('https://www.tripadvisor.com.sg/Attractions-g298112-Activities-oa%s-Gifu_Gifu_Prefecture_Tokai_Chubu.html', 5, 'Gifu'),
-    ('https://www.tripadvisor.com.sg/Attractions-g298106-Activities-oa%s-Nagoya_Aichi_Prefecture_Tokai_Chubu.html', 16, 'Nagoya'),
 ]
 
 
@@ -35,11 +35,35 @@ def write(html, filename):
 
 
 def write_excel(filename, alldata, flag=None):
+    filename = 'data/' + filename
     if flag:
-        filename = filename.replace('.xls', '_'+str(flag)+'.xls')
+        filename = filename.replace('.xls', '_' + str(flag) + '.xls')
     d = os.path.dirname(filename)
     if not os.path.exists(d):
         os.makedirs(d)
+
+    i = 0
+    while len(alldata) > 65500:
+        _filename = filename.replace('.xls', '_%s.xls' % i)
+        start_index = 0
+        end_index = 65500
+        data = alldata[start_index:end_index]
+        alldata = alldata[end_index:]
+        w = xlwt.Workbook(encoding='utf-8')
+        ws = w.add_sheet('old', cell_overwrite_ok=True)
+        for row in range(0, len(data)):
+            one_row = data[row]
+            for col in range(0, len(one_row)):
+                try:
+                    ws.write(row, col, one_row[col][:32766])
+                except:
+                    try:
+                        ws.write(row, col, one_row[col])
+                    except:
+                        print('===Write excel ERROR===' + str(one_row[col]))
+        w.save(_filename)
+        print("%s===========over============%d" % (_filename, len(data)))
+        i += 1
     w = xlwt.Workbook(encoding='utf-8')
     ws = w.add_sheet('old', cell_overwrite_ok=True)
     for row in range(0, len(alldata)):
@@ -51,9 +75,9 @@ def write_excel(filename, alldata, flag=None):
                 try:
                     ws.write(row, col, one_row[col])
                 except:
-                    print('===Write excel ERROR===', str(one_row[col]))
+                    print('===Write excel ERROR===' + str(one_row[col]))
     w.save(filename)
-    print(filename+"===========over============")
+    print("%s===========over============%d" % (filename, len(alldata)))
 
 
 def get_category_urls(url, is_second_level=False):
@@ -92,10 +116,9 @@ def request_sheet1(category, url):
                 can_booking = 1
             location, description, extra = get_attraction_details(link)
             one_row = [hotel_id, link, name, location, avg_rating, review_number, can_booking, description] + extra
-            print(one_row)
             sheet1_data.append(one_row)
             if review_number > 0:
-                request_sheet2(hotel_id, review_number, link)
+                request_sheet2(hotel_id, review_number, link, R_ID)
             R_ID += 1
         except:
             raise
@@ -164,11 +187,14 @@ def get_user_location(ori):
         reg = 'userLocation">(.*?)<'
         data = re.compile(reg).findall(ori)
         return data[0]
+    if 'strong' in ori:
+        reg = 'strong>(.*?)<'
+        data = re.compile(reg).findall(ori)
+        return data[0]
     return ''
 
-def request_sheet2(hotel_id, number, hotel_url):
-    global sheet2_data,  sheet3_data
-    page_no = number / 5
+def request_sheet2(hotel_id, number, hotel_url, R_ID):
+    page_no = number / 10
     if number % 10 != 0:
         page_no += 1
     for i in range(0, page_no):
@@ -176,24 +202,27 @@ def request_sheet2(hotel_id, number, hotel_url):
             break
         try:
             url = get_review_url(hotel_url, i)
-            html = get_request(url)
-            print('sheet2', page_no, i, url)
-            reg = 'avatar profile_(.*?)".*?onclick=.*?>(.*?)<(.*?)UID.*?ui_bubble_rating bubble_(.*?)".*?class="entry">(.*?)</div'
-
-            comment_list = re.findall(reg, html)
-            for comment in comment_list:
-                username = comment[1]
-                location = get_user_location(comment[2])
-                uid = comment[0]
-                rating = comment[3]
-                text = remove_html_tag(comment[4])
-                level, travel_style, user_url = get_level_of_uid(uid)
-                one_row = [hotel_id, hotel_url, username, rating[0], text, location, level, travel_style]
-                sheet2_data.append(one_row)
-                print(one_row)
-                sheet3_data.append([uid, user_url])
+            print('sheet2', R_ID, page_no, i, url)
+            request_one_sheet2(hotel_id, hotel_url, url)
         except Exception as e:
             print('ERROR-sheet2-', hotel_id, i, e)
+
+def request_one_sheet2(hotel_id, hotel_url, url):
+    global sheet2_data,  sheet3_data
+    html = get_request(url)
+    reg = 'avatar profile_(.*?)".*?onclick=.*?">(.*?)</div(.*?)ui_bubble_rating bubble_(.*?)".*?class="entry">(.*?)</div'
+
+    comment_list = re.findall(reg, html)
+    for comment in comment_list:
+        username = remove_html_tag(comment[1])
+        location = get_user_location(comment[2])
+        uid = comment[0]
+        rating = comment[3]
+        text = remove_html_tag(comment[4]).replace('.More', '')
+        level, travel_style, user_url = get_level_of_uid(uid)
+        one_row = [hotel_id, hotel_url, username, rating[0], text, location, level, travel_style]
+        sheet2_data.append(one_row)
+        sheet3_data.append([uid, user_url])    
 
 def get_level_of_uid(uid):
     if uid_level_dict.get(uid):
@@ -260,3 +289,4 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 get_all()
+# request_one_sheet2('1', '2', 'https://www.tripadvisor.com.sg/Attraction_Review-g298566-d1425051-Reviews-or40-Kids_Plaza_Osaka-Osaka_Osaka_Prefecture_Kinki.html')
